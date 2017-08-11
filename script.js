0a030adcbb6716d8a9fb5a1101219577bc5c9aeb
aj28293 <aj28293@gmail.com>
2017-08-11 11:35:22 -0700

    Fix script.js
    Fix script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ WARNING!!! WARNING!!!
   // using the 'speak' method or either helloSpeaker's or byeSpeaker's
   // 'speak' method.
   // See Lecture 50, part 1
-  for (var x = 0; i < names.length; x++) {
+  for (var i = 0; i < names.length; i++) {
 
     // STEP 11:
     // Retrieve the first letter of the current name in the loop.
@@ -50,15 +50,13 @@ WARNING!!! WARNING!!!
     // string object's 'toLowerCase' method on the result so we can compare
     // to lower case character 'j' afterwards.
     // Look up these methods on Mozilla Developer Network web site if needed.
-    var firstLetter = names[x].charAt(0).toLowerCase();
+    var firstLetter = names[i].charAt(0).toLowerCase();
 
     // STEP 12:
     // Compare the 'firstLetter' retrieved in STEP 11 to lower case
     // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
     // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
     // name in the loop.
-    if (firstLetter == 'j') {
-      byeSpeaker.speak(names[x])} 
-	else {
-      helloSpeaker.speak(names[x])  }  }
+    if (firstLetter == 'j') {byeSpeaker.speak(names[i])} 
+	else {helloSpeaker.speak(names[i])} }
 })();
